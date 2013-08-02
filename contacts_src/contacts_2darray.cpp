a7a9d0a8d3855d8242c800a10734c899fa5d1bcc
Josh Wainwright <wainwright.ja@gmail.com>
2013-08-02 17:06:21 +0100

    File encoding correction added to program

--- contacts_src/contacts_2darray.cpp
+++ contacts_src/contacts_2darray.cpp
@@ -12,13 +12,14 @@
 //#include <unistd.h>
 
 //#define GOOGLECSV "/home/josh/.mutt/google.csv"
-#define GOOGLECSV "/home/josh/Downloads/google.csv"
 
 using namespace std;
 
 int menu();
+int countSubstring(const string& str, const string& sub);
 
 int main(int argc, char* argv[]) {
+string GOOGLECSV = "/home/josh/Downloads/google.csv";
 
 	int Continue = 1;
 	string Search;
@@ -26,13 +27,17 @@ int main(int argc, char* argv[]) {
 	vector<int> resultsArray;
 	string line;
 	bool first=true;
+	ofstream testout("testout");
 
 	do{
-		ifstream google(GOOGLECSV);
+		ifstream google(GOOGLECSV.c_str());
 		if (!google) {
 			cout << "Cannot open input file" << endl;
 			return 1;
 		}
+
+		string command = "iconv -f $(file -b --mime-encoding " + GOOGLECSV + ") -t UTF-8 " + GOOGLECSV + " > tmp && mv tmp " + GOOGLECSV;
+		system(command.c_str());
 
 		ContactsArrayFull.clear();
 		resultsArray.clear();
@@ -46,12 +51,23 @@ int main(int argc, char* argv[]) {
 			Search = argv[1];
 		}
 
+		/*----------------------------------------------------------------
+		------------------ Contacts array generation ---------------------
+		----------------------------------------------------------------*/
 		//Until the end of the file, split each line into a vector, place each
 		//line-vector into a global vector.
 		int entryNumber=0;
+		string line2;
 		while(!google.eof()){
 			getline(google,line);
 
+			//Deal with lines that are split by internal newlines
+			while (countSubstring(line, ",") < 84){
+				getline(google, line2);
+				line = line + line2;
+			}
+
+			//Add new contact
 			ContactsArrayFull.push_back(vector <string> ());
 
 			string delimiter = ",";
@@ -61,6 +77,7 @@ int main(int argc, char* argv[]) {
 			while ((pos = line.find(delimiter)) != string::npos) {
 				fieldContents = line.substr(0, pos);
 
+				//Add new field, and populate, to the new contact added above
 				ContactsArrayFull[entryNumber].push_back(fieldContents);
 				line.erase(0, pos + delimiter.length());
 
@@ -68,6 +85,18 @@ int main(int argc, char* argv[]) {
 			entryNumber++;
 		}
 
+		//Debuging - print out complete contact array
+		/*for (unsigned int i = 0; i < ContactsArrayFull.size()-1; ++i){
+
+			for (unsigned int j = 0; j < ContactsArrayFull[i].size()-1; ++j){
+					testout << ContactsArrayFull[i][j] << "\t";
+			}
+			testout << endl;
+		}*/
+
+		/*----------------------------------------------------------------
+		-------------------------- Search --------------------------------
+		----------------------------------------------------------------*/
 		int counter=0;
 		int found = 0;
 		cout << "Total size = " << ContactsArrayFull.size() << endl;
@@ -88,6 +117,7 @@ int main(int argc, char* argv[]) {
 		}
 
 		if (resultsArray.size() == 0){
+			//No results found
 			cout << "Search not found, please try again." << endl;
 			google.clear();
 			google.seekg(0);
@@ -99,13 +129,71 @@ int main(int argc, char* argv[]) {
 				cin >> pickResult;
 				R = resultsArray[pickResult];
 			}else{
+				//One result found
 				R = resultsArray[0];
 			}
-
 			cout << endl;
-			for (int i = 0; i < 83; ++i) {
+
+			/*-----------------------------------------------------------
+			-----------------Print to Screen ----------------------------
+			-----------------------------------------------------------*/
+			for (unsigned int i = 0; i < ContactsArrayFull[R].size(); ++i) {
 				if (ContactsArrayFull[R][i] != ""){
-					cout << left << setw(17) << ContactsArrayFull[0][i] << " = " << ContactsArrayFull[R][i] << endl;
+					if (i < 27){
+						//Standard info
+						cout << i << "  "  << left << setw(17) << ContactsArrayFull[0][i]
+							<< " = " << ContactsArrayFull[R][i] << endl;
+					}else if (i < 41){
+						//Emails
+						if(i==27)cout << endl;
+						cout << i << "  "  << "Email " << left << setw(11) << ContactsArrayFull[R][i]
+							<< " = " << ContactsArrayFull[R][i+1] << endl;
+						i++;
+					}else if (i < 44){
+						//IM
+						if(i==41)cout << endl;
+						cout << i << "  "  << left << setw(17) << ContactsArrayFull[0][i]
+							<< " = " << ContactsArrayFull[R][i] << endl;
+					}else if (i < 52){
+						//Phones
+						if(i==44)cout << endl;
+						cout << i << "  "  << "Phone " << left << setw(11) << ContactsArrayFull[R][i]
+							<< " = " << ContactsArrayFull[R][i+1] << endl;
+						i++;
+					}else if (i < 70){
+						//Address
+						if(i==52)cout << endl;
+						cout << i << "  "  << left << setw(17) << ContactsArrayFull[0][i]
+							<< " = " << ContactsArrayFull[R][i] << endl;
+					}else if (i < 78){
+						//Organisation
+						if(i==70)cout << endl;
+						cout << i << "  "  << left << setw(17) << ContactsArrayFull[0][i]
+							<< " = " << ContactsArrayFull[R][i] << endl;
+					}else if (i < 80){
+						//Relation
+						if(i==78)cout << endl;
+						cout << i << "  "  << "Relation " << left << setw(11) << ContactsArrayFull[R][i]
+							<< " = " << ContactsArrayFull[R][i+1] << endl;
+						i++;
+					}else if (i < 82){
+						//Website
+						if(i==80)cout << endl;
+						cout << i << "  "  << "Website " << left << setw(11) << ContactsArrayFull[R][i]
+							<< " = " << ContactsArrayFull[R][i+1] << endl;
+						i++;
+					}else if (i < 84){
+						//Event
+						if(i==82)cout << endl;
+						cout << i << "  "  << "Event " << left << setw(11) << ContactsArrayFull[R][i]
+							<< " = " << ContactsArrayFull[R][i+1] << endl;
+						i++;
+					}else{
+						//Everything else
+						if(i==84)cout << endl;
+						cout << left << setw(17) << ContactsArrayFull[0][i]
+							<< " = " << ContactsArrayFull[R][i] << endl;
+					}
 				}
 			}
 			Continue = menu();
@@ -126,4 +214,20 @@ int menu(){
 	cin >> option;
 
 	return option;
+}
+
+/******************************************************************************
+ *                                 Functions                                  *
+ ******************************************************************************/
+
+int countSubstring(const string& str, const string& sub)
+{
+    if (sub.length() == 0) return 0;
+    int count = 0;
+    for (size_t offset = str.find(sub); offset != string::npos;
+	 offset = str.find(sub, offset + sub.length()))
+    {
+        ++count;
+    }
+    return count;
 }
