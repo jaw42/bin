fe5b960174228c659d526ec4d729481026c7fb73
Josh Wainwright <josh.wainwright@ldra.com>
2016-04-29 13:46:02 +0100

    [fwatch] script to watch files and perform some action when they change

---
+++ fwatch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+opt=false
+for a in $@; do
+	if [ "$a" == "--" ]; then
+		opt=true
+		continue
+	fi
+	if $opt; then
+		cmd="$cmd $a"
+	else
+		files="$files $a"
+	fi
+done
+
+if [ -z "$files" ]; then
+    echo "Nothing to watch, abort"
+    exit
+else
+    echo "watching: $files"
+    echo "command:  $cmd"
+fi
+
+previous_checksum="dummy"
+while [ 1 ]; do
+    # I use Mac so I have `md5`, in linux it's `md5sum`
+    checksum=$(cksum $files | cksum)  
+    if [ "$checksum" != "$previous_checksum" ]; then
+		date
+        eval "$cmd"
+    fi
+    previous_checksum="$checksum"
+    sleep 1
+done
+
+
