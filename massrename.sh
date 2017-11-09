d2e184f24a9f9cec5c34390ab32f68528ee0101c
Josh Wainwright <josh@exonar.com>
2017-11-09 11:53:27 +0000

    Accept file list passed to stdin

--- massrename.sh
+++ massrename.sh
@@ -1,20 +1,45 @@
 #!/bin/sh
 # Created:  Tue 07 Nov 2017
-# Modified: Tue 07 Nov 2017
+# Modified: Thu 09 Nov 2017
 # Author:   Josh Wainwright
 # Filename: massrename.sh
+if [ "$1" = "-h" ]; then
+	cat <<EOF
+Rename files and folders using $EDITOR (\$EDITOR)
 
-path=${1:-.}
-list=$(find "$path" -maxdepth 1 -type f)
+$ massrename.sh PATH
+	Selects all files in specified PATH
+
+$ find -type f | massrename.sh
+	Selects all newline separated files or folders passed to stdin
+EOF
+	exit
+fi
+
+if [ $# -gt 0 ]; then
+	path=${1}
+	list=$(find "$path" -maxdepth 1 -type f)
+else
+	list=$(cat)
+	printf 'Reading from STDIN\n' 1>&2 
+fi
 
 tmpf=$(mktemp)
 printf "$list\n" > "$tmpf.orig"
 printf "$list\n" > "$tmpf"
 
+count_before=$(wc -l < "$tmpf.orig")
+printf '%i files selected\n' "$count_before" 1>&2
+
 $EDITOR "$tmpf"
 
-if [ "$(wc -l < "$tmpf.orig")" -ne "$(wc -l < "$tmpf")" ]; then
+if [ "$count_before" -ne "$(wc -l < "$tmpf")" ]; then
 	echo "Don't add or remove lines"
+	exit
+fi
+
+if cmp -s "$tmpf.orig" "$tmpf"; then
+	echo "No changes made."
 	exit
 fi
 
